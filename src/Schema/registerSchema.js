b66022517204320855a9a7001ca0bda5cfb0c9ad
NesmaFarid9 <nesmafarid45@gmail.com>
2025-09-06 21:15:30 +0300

    edit

--- src/Schema/registerSchema.js
+++ src/Schema/registerSchema.js
@@ -7,18 +7,21 @@ export const schema = zod.object({
 
     email: zod.string().nonempty('Email is required')
             .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,15}$/, 'Email is invalid'),
+
     password: zod.string().nonempty('Password is required')
-                .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/, 'Password is inValid'),
+            .regex(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/, 'Password is invalid'),
 
     rePassword: zod.string().nonempty("Confirm password is required"),
 
-    dateOfBirth: zod.coerce.date('Date is required').refine((value) => {
-        const yearOfBirth = value.getFullYear();
-        const currentYear = new Date().getFullYear();
-        const age = currentYear - yearOfBirth;
-        return age >= 18
-    }, {message: 'Age should be greater than 18'}),
+    dateOfBirth: zod.string().nonempty('Date is required')
+            .pipe(zod.coerce.date())
+            .refine((value) => {
+                const yearOfBirth = value.getFullYear();
+                const currentYear = new Date().getFullYear();
+                const age = currentYear - yearOfBirth;
+                return age >= 18;
+            }, { message: 'Age should be greater than 18' }),
 
     gender: zod.string().nonempty('Gender is required'),
 
-}).refine((data)=> data.password === data.rePassword, {path: ['rePassword'], message: 'Password and confirm password not match'});
+    }).refine((data)=> data.password === data.rePassword, {path: ['rePassword'], message: 'Password and confirm password not match'});
